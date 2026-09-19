e77f628c26bd237bda9f09f906e6dfa8b5fac1b8
ewanyonyi <wanyonyi.d.emanuel@gmail.com>
2026-09-19 20:29:32 +0300

    release: v1.1.1 (2026091900)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_schola_timetabler';
-$plugin->version   = 2026091100;
+$plugin->version   = 2026091900;
 $plugin->requires  = 2024042200;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.1.0';
+$plugin->release   = '1.1.1';
